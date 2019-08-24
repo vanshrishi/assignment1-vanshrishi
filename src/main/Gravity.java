9b8a5e5803212d2b52924d98aaaf2283ddf647a1
vanshrishi <vansh.rishi_cs18@gla.ac.in>
2019-08-24 11:36:40 -0700

    creating object .

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -3,9 +3,13 @@
  * @version 1.0
  * */
 package main;
+
+import java.util.Scanner;
+
 public class Gravity {
 	public static void main(String[] args) {
 		double distance =0,time=0,speed=0,g=9.8;
+		Scanner t = new Scanner(System.in);
 
 	}
 }
